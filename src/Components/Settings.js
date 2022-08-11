0048c477a55d4038b96ad6bf681ccb0939e503e2
Zeno Driesen <zeno.driesen@gmail.com>
2022-08-11 10:21:57 +0200

    getting the categories displayed getting the categories displayed in a select drop down, but kinda stuck here. Thinking of first starting it simple, so I will get back to this later. Just want to directly generate random questions and will build on from there.

--- src/Components/Settings.js
+++ src/Components/Settings.js
@@ -1,28 +1,59 @@
 import React, {useEffect, useState} from "react";
 
 function Settings() {
+
     //useState hook
     const[options, setOptions] = useState(null);
 
+    const [category, setCategory] = useState("");
+
+    const [loading, setLoading] = useState(false);
+
     //useEffect hook
     useEffect(() => {
-        //declare the async data fetching function
-        const fetchData = async () => {
-            function sleep(ms){
-                return new Promise(resolve => setTimeout(resolve, ms));
-            }
 
-            //waits for 10000ms
-            await sleep(1000);
-            return 'Hello World';
-        };
+        setLoading(true);
 
-        const result = fetchData()
-        //make sure to catch any error
-            .catch(console.error);
+        async function fetchCategories() {
+            const apiUrl = 'https://opentdb.com/api_category.php';
+            let response = await fetch(apiUrl);
+            let categories = await response.json();
+            return categories;
+        }
 
-        console.log(result)
-    }, [setOptions])
+        fetchCategories().then((categories) => {
+            setLoading(false);
+            setOptions(categories.trivia_categories);
+        });
+
+    }, [setOptions]);
+
+    const handleCategory = event => {
+        setCategory(event.target.value)
+    }
+
+    if(!loading){
+
+        return (
+            <div>
+                <div>
+                    <h2>Select a category:</h2>
+                    <select value={category} onChange={handleCategory}>
+                        <option>Discover them here</option>
+                        {options && options.map((option) => (
+                            <option value={option.id} key={option.id}>
+                                {option.name}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+            </div>
+        );
+
+    } else {
+        <p>LOADING...</p>
+    }
+
 }
 
 export default Settings;
